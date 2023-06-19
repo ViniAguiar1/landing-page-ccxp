8b968226e27e672b807ba6f7adc0967b9c834ceb
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-06-18 21:30:02 -0300

    Atualização

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,52 @@ function formato(tempo) {
 
 countDown()
 setInterval(countDown, 1000)
+
+function highlightCard(selector){
+    let element = document.querySelector(selector)
+    element.classList.toggle("card-highlight")
+}
+
+function checkKeyboardCode(){
+    document.addEventListener('keydown', (event) =>{
+        let name = event.key
+        let code = event.code
+        // Alert the key name and key code on keydown
+        alert(`Tecla pressionada ${name} \r\n Key code value: ${code}`)
+    }, false)
+}
+
+function addKeyboardEventListener(){
+    document.addEventListener('keydown', (event) => {
+        let ingresso1 = document.getElementById("quinta")
+        let ingresso2 = document.getElementById("sexta")
+        let ingresso3 = document.getElementById("sabado")
+        let ingresso4 = document.getElementById("domingo")
+
+        let code = event.code
+        if(code == 'Digit1'){
+            ingresso1.classList.toggle("card-highlight")
+            ingresso2.classList.remove("card-highlight")
+            ingresso3.classList.remove("card-highlight")
+            ingresso4.classList.remove("card-highlight")
+        } if(code == 'Digit2'){
+            ingresso1.classList.remove("card-highlight")
+            ingresso2.classList.toggle("card-highlight")
+            ingresso3.classList.remove("card-highlight")
+            ingresso4.classList.remove('card-highlight')
+        } if(code == 'Digit3') {
+            ingresso1.classList.remove("card-highlight")
+            ingresso2.classList.remove("card-highlight")
+            ingresso3.classList.toggle("card-highlight")
+            ingresso4.classList.remove("card-highlight")
+        } if (code == 'Digit4'){
+            ingresso1.classList.remove("card-highlight")
+            ingresso2.classList.remove("card-highlight")
+            ingresso3.classList.remove("card-highlight")
+            ingresso4.classList.toggle("card-highlight")
+        }
+    }, false)
+}
+
+// checkKeyboardCode()
+addKeyboardEventListener()
